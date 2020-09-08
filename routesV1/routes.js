fb2b3def57d146141538c3c3674172d8b981b88b
Marc Lytle <mlytle4218@gmail.com>
2020-09-07 17:30:13 -0700

    Add checking for bearere token to route.js

--- routesV1/routes.js
+++ routesV1/routes.js
@@ -1,15 +1,38 @@
 let express = require('express');
 let router = express.Router();
-let BearerToken = require('express-bearer-token');
+let bearerToken = require('express-bearer-token');
+let jwt = require('jsonwebtoken');
 let winston = require('../logs/winston');
+
 
 // local code imports
 let userController = require('../controllers/userController');
-winston.debug('in router')
-winston.debug(router)
+
+let checkForBearerToken = (req, res, next) =>{
+    if (req.token) {
+        jwt.verify(req.token, process.env.SECRET_KEY, (error, decoded) =>{
+            if (error){
+                return res.json({'success':false, 'message': 'failed to authenticate'});
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        })
+    } else {
+        winston.error('No token found in request');
+        return res.status(403).send({
+            'success': false,
+            'message': 'No token found in request'
+        })
+    }
+}
+
 
 // begin router controls
 router.post('/user/authenticate', userController.signInUser);
+router.use(bearerToken())
+router.use(checkForBearerToken)
+
 
 
 module.exports = router;
