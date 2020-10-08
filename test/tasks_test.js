12bd7f044726b3ebf21d42e6e783f6522f4d5c27
mlytle4218 <mlytle4218@gmail.com>
2020-10-07 20:28:45 -0700

    do not know

--- test/tasks_test.js
+++ test/tasks_test.js
@@ -7,30 +7,71 @@ const { response } = require('express');
 let should = chai.should();
 
 chai.use(chaiHttp);
-describe('User authentication', () => {
+describe('Task test', () => {
+    let userToken;
+    before((done) => {
+        let user = {
+            username: 'corp\bob',
+            password: '123'
+        };
+        chai.request(server)
+            .post('/api/v1/user/authenticate')
+            .send(user)
+            .end((error, response) => {
+                console.log(response.body.token);
+                userToken = response.body.token
+                done();
+            })
+    })
     beforeEach((done) => {
         done();
-    })   
+    })
+    /*
+    * dummy test
+    */
+    describe('dummy test', () => {
+        it('it should do nothing', (done) => {
+            done();
+        })
+    });
     /*
     test that it gets all the tasks avaiable, for now.
     */
-    // describe('/GET tasks', () => {
-    //     it('it should GET all tasks', (done) => {
-    //         chai.request(server)
-    //         .get('/book')
-    //         .end((error, response) => {
-    //             response.should.have.status(200);
-    //             response.body.should.be.a('array');
-    //             response.body.length.should.be.eql(0);
-    //             done();
-    //         });
-    //     });
-    // });
-    // /*
-    // test that it creates a new task
-    //  */
-    // describe('/POST task', () => {
-    //     it('it should ')
-    // })
+    describe('/GET tasks', () => {
+        it('it should GET all tasks', (done) => {
+            chai.request(server)
+                .get('api/v1/task')
+                .end((error, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a('array');
+                    response.body.length.should.be.eql(0);
+                    done();
+                });
+        });
+    });
+    /*
+    test that it creates a new task
+     */
+    describe('/POST task', () => {
+        it('it should create a task', (done) => {
+            let task = {
+                'name':'task name'
+            }
+            let body = {
+                "token":userToken,
+                "task":task
+            }
+            chai.request(server)
+            
+            .post('/api/v1/task')
+            .send(body)
+            .end((err, res) => {
+                response.should.have.status(200);
+                response.body.should.have.property('task');
+                response.body.task.should.be.a('object');
+
+            })
+        })
+    })
 
 });
